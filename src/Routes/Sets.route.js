4ee1324f0b3fb9df26c7218b418a5f63568d2b14
Guillaume Duramé <durameg@gmail.com>
2024-06-05 15:58:40 +0200

    vant test wishlist

--- src/Routes/Sets.route.js
+++ src/Routes/Sets.route.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const Set = require("../Models/Set");
 
 router.get("/", (req, res) => {
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+  const skipIndex = (page - 1) * limit;
   Set.find()
+    .skip(skipIndex)
+    .limit(limit)
     .then((sets) => res.json(sets))
     .catch((err) => res.status(500).json({ error: err.message }));
 });
