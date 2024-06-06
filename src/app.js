b6761374d11b98ddf69c86f21fba5dc1b7dc14db
Guillaume Duramé <durameg@gmail.com>
2024-06-06 18:09:25 +0200

    deploy

--- src/app.js
+++ src/app.js
@@ -8,11 +8,11 @@ const postRouter = require("./Routes/Post.route");
 const wishlistRouter = require("./Routes/Wishlist.route");
 const setsRouter = require("./Routes/Sets.route");
 const cors = require("cors");
-const { CORS_ORIGIN } = require("./const");
+const { CORS_ORIGIN, MONGO_URI } = require("./const");
 const InventoryPart = require("./Models/InventoryPart");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/projet-3")
+  .connect(MONGO_URI)
   .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
   .catch((err) => console.error("Error connecting to MongoDB", err));
 
