ef31d04fe3222c60861bf6bc37f955c490d8e03b
Guillaume Duramé <durameg@gmail.com>
2024-06-06 17:31:44 +0200

    deploy

--- src/const.js
+++ src/const.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
-const TOKEN_SECRET = "secret";
+const TOKEN_SECRET = "dsfsdfsadgdffgdhs";
 const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5000";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/projet-3";
+const PORT = process.env.PORT || 5000;
 
-module.exports = { CORS_ORIGIN, TOKEN_SECRET };
+module.exports = { CORS_ORIGIN, TOKEN_SECRET, MONGO_URI, PORT };
